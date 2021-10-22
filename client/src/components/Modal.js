99b29cba1ee892d5454fe2c75593c02d90fdcf89
eminmuratoglu <eminm.6161@gmail.com>
2021-10-22 04:50:46 -0400

    react modal installed and component created

---
+++ client/src/components/Modal.js
@@ -0,0 +1,25 @@
+// import React, { Component } from 'react'
+
+// export class Modal extends Component {
+// 	state = {
+// 		show: false
+// 	};
+// 	handleModal = () => {
+// 		this.setState({ show: !this.state.show });
+// 	};
+//   render() {
+//     return (
+//       <div>
+// 				<Modal isOpen={this.state.show}>
+// 					<p>
+// 						Get a random tweet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quaerat quia. Pariatur
+// 						quas numquam tenetur eum facere doloribus iste suscipit repellendus magnam laboriosam. Repudiandae corporis
+// 						expedita, incidunt ab unde obcaecati.
+// 					</p>
+// 				</Modal>
+//       </div>
+//     )
+//   }
+// }
+
+// export default Modal
