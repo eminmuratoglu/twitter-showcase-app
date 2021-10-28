7ffe973caba00f3ff9c3e5f9f7da71a9a669d7e4
eminmuratoglu <eminm.6161@gmail.com>
2021-10-28 03:03:04 -0400

    modal added

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,25 +1,34 @@
-// import React, { Component } from 'react'
+import React from 'react';
 
-// export class Modal extends Component {
-// 	state = {
-// 		show: false
-// 	};
-// 	handleModal = () => {
-// 		this.setState({ show: !this.state.show });
-// 	};
-//   render() {
-//     return (
-//       <div>
-// 				<Modal isOpen={this.state.show}>
-// 					<p>
-// 						Get a random tweet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quaerat quia. Pariatur
-// 						quas numquam tenetur eum facere doloribus iste suscipit repellendus magnam laboriosam. Repudiandae corporis
-// 						expedita, incidunt ab unde obcaecati.
-// 					</p>
-// 				</Modal>
-//       </div>
-//     )
-//   }
-// }
+function Modal(props) {
+	return (
+		<div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div className="modal-dialog modal-dialog-centered">
+				<div className="modal-content">
+					{/* <div className="modal-header">
+							<h5 className="modal-title" id="exampleModalLabel">
+								Modal title
+							</h5>
+							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+						</div> */}
+					<div className="modal-body p-2">
+						{props.children}
+						<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+							Close
+						</button>
+					</div>
+					{/* <div className="modal-footer">
+							<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+								Close
+							</button>
+							<button type="button" className="btn btn-primary">
+								Save changes
+							</button>
+						</div> */}
+				</div>
+			</div>
+		</div>
+	);
+}
 
-// export default Modal
+export default Modal;
--- client/src/components/RandomTweets.js
+++ client/src/components/RandomTweets.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './RandomTweet.css';
 import axios from 'axios';
-import Modal from 'react-modal';
+// import Modal from 'react-modal';
+import Modal from './Modal';
+import huntingImg from './images/hunting.png';
 import chesscomImg from './images/chess.com.png';
 import javascriptImg from './images/javascript.png';
 import nbaImg from './images/nba.png';
@@ -9,49 +11,74 @@ import reactImg from './images/react.png';
 import uclImg from './images/ucl.png';
 import TweetByUser from './TweetByUser';
 
-// work on modal
-
 class RandomTweets extends Component {
 	state = {
-		tweet: '',
-		show: false
+		tweet: ''
 	};
 	getRandom = (tweetsArray) => {
 		return tweetsArray[Math.floor(Math.random() * tweetsArray.length)];
 	};
 
-	setAndShowTweet = async () => {
-		this.setState({ show: !this.state.show });
-		const resp = await axios.get(`api/tweets/user/?searchQuery=@NBA`);
+	setAndShowTweet = async (e) => {
+		const resp = await axios.get(`api/tweets/user/?searchQuery=${e.target.name}`);
 		let randomTweet = this.getRandom(resp.data);
 		this.setState({ tweet: randomTweet });
-	};
-
-	toggleModal = () => {
-		this.setState({ show: !this.state.show });
 	};
 
 	render() {
 		return (
 			<div className="randomTweet__container">
-				<div style={{ fontSize: '12rem' }}>🎯</div>
-				<p>
-					Get a random tweet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quaerat quia. Pariatur
-					quas numquam tenetur eum facere doloribus iste suscipit repellendus magnam laboriosam. Repudiandae corporis
-					expedita, incidunt ab unde obcaecati.
-				</p>
-				<div className="images-container">
-					<img name="nba" onClick={this.setAndShowTweet} src={nbaImg} alt="NBA" />
-					<img name="chess.com" src={chesscomImg} alt="chess.com" />
-					<img name="ucl" src={uclImg} alt="ucl" />
-					<img name="react" src={reactImg} alt="React" />
-					<img name="js" src={javascriptImg} alt="JavaScript" />
+				<div className="randomTweet__header">
+					<img src={huntingImg} style={{ width: '12rem' }} alt="target" />
+					<div>
+						<h4>Click on one of my favorite Twitter topics below</h4>
+						<h4>And "catch" a random tweet!</h4>
+					</div>
 				</div>
-				{/* {this.state.tweet !== '' && <TweetByUser tweets={this.state.tweet} />} */}
-				<Modal isOpen={this.state.show}>
-					<button onClick={this.toggleModal}>X</button>
-					{this.state.tweet !== '' && <TweetByUser tweets={this.state.tweet} />}
-				</Modal>
+				<div className="images-container">
+					<img
+						name="nba"
+						onClick={this.setAndShowTweet}
+						data-bs-toggle="modal"
+						data-bs-target=".modal"
+						src={nbaImg}
+						alt="NBA"
+					/>
+					<img
+						name="chess.com"
+						onClick={this.setAndShowTweet}
+						data-bs-toggle="modal"
+						data-bs-target=".modal"
+						className="chess-pic"
+						src={chesscomImg}
+						alt="chess.com"
+					/>
+					<img
+						name="uefa champions league"
+						onClick={this.setAndShowTweet}
+						data-bs-toggle="modal"
+						data-bs-target=".modal"
+						src={uclImg}
+						alt="ucl"
+					/>
+					<img
+						name="reactjs"
+						onClick={this.setAndShowTweet}
+						data-bs-toggle="modal"
+						data-bs-target=".modal"
+						src={reactImg}
+						alt="React"
+					/>
+					<img
+						name="javascript"
+						onClick={this.setAndShowTweet}
+						data-bs-toggle="modal"
+						data-bs-target=".modal"
+						src={javascriptImg}
+						alt="JavaScript"
+					/>
+				</div>
+				<Modal className="modal">{this.state.tweet !== '' && <TweetByUser tweets={this.state.tweet} />}</Modal>
 			</div>
 		);
 	}
