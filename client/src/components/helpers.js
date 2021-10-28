76d382258b259eb91a5a22581d191741d8b09e19
eminmuratoglu <eminm.6161@gmail.com>
2021-10-28 03:04:38 -0400

    formatdate and getVideo helper funcs

---
+++ client/src/components/helpers.js
@@ -0,0 +1,19 @@
+export function getMediaEl(tw) {
+	if (tw.extended_entities) {
+		let media = tw.extended_entities.media[0];
+		if (media.type === 'photo') {
+			return <img src={media.media_url} width="300" />;
+		} else if (media.type === 'video') {
+			function filterAndGetSmallestMp4() {
+				return media.video_info.variants
+					.filter((v) => v.content_type === 'video/mp4')
+					.sort((a, b) => a.bitrate > b.bitrate)[0].url;
+			}
+			return <video src={filterAndGetSmallestMp4()} width="320" height="240" controls="controls" />;
+		}
+	}
+}
+
+export function formatDate(date) {
+	return `${date.split(' ').slice(1, 3).join(' ')} ${date.slice(-4)}`;
+}
