16d2e6caaeb6eb4470d880f92f5a3543d10a8726
eminmuratoglu <eminm.6161@gmail.com>
2021-10-28 21:24:31 -0400

    Home page componentadded. logo added

---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import twitterBirds from './images/twitter-birds.png';
+import twitterBlack from './images/twitter-black.png';
+
+export class Home extends Component {
+	render() {
+		return (
+			<div>
+				<img src={twitterBlack} alt="twitter birds" style={{}} />
+			</div>
+		);
+	}
+}
+
+export default Home;
